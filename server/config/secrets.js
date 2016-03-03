10078b6a84ace073bacb33aeacb160c9a5136701
pmannle <phil_mannle@yahoo.com>
2016-03-03 10:14:33 -0800

    updating default environment variables

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -5,24 +5,15 @@ module.exports = {
   sessionSecret: process.env.SESSION_SECRET || 'change this',
 
   mailgun: {
-    user: process.env.MAILGUN_USER || 'postmaster@sandbox7fb3dd8753e446728824566da833f16a.mailgun.org',
-    password: process.env.MAILGUN_PASSWORD || 'password',
-    apiKey: process.env.MAILGUN_API_KEY || 'key-06b5e5245f74052fdae305818b3fb686',
-    domain: process.env.MAILGUN_DOMAIN || 'sandbox7fb3dd8753e446728824566da833f16a.mailgun.org'
+    user: process.env.MAILGUN_USER || '',           // your mailgun from address here
+    password: process.env.MAILGUN_PASSWORD || '',   // mailgun password
+    apiKey: process.env.MAILGUN_API_KEY || '',      // mailgun API key
+    domain: process.env.MAILGUN_DOMAIN || ''        // mailgun sandbox/domain
   },
 
   stripeOptions: {
-    apiKey: process.env.STRIPE_KEY || 'sk_test_gPV7zXKrLSIVOKXJxZIBk0Wj',
-    stripePubKey: process.env.STRIPE_PUB_KEY || 'pk_test_TtP0GremyqcCDrfW0739NJM8',
-    defaultPlan: 'free',
-    plans: ['free'],
-    planData:
-    {
-      'free': {
-        name: 'Free',
-        amount: 0
-      }
-    }
+    apiKey: process.env.STRIPE_KEY || '',           // stripe test or prod key
+    stripePubKey: process.env.STRIPE_PUB_KEY || '', // stripe test or pord public key
   },
 
   googleAnalytics: process.env.GOOGLE_ANALYTICS || ''
